fa5aa6aaaaceee949988d10a63650af5402c8039
serhii-sheiko <128324762+serhii-sheiko@users.noreply.github.com>
2023-05-13 13:02:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Hello World! {datetime.now()} version 3'
+    return f'Hello World! {datetime.now()} version 4'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
