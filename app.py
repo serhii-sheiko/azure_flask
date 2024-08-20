a28c357cd6527c183d0b22497bb982f438daf0ef
serhii-sheiko <128324762+serhii-sheiko@users.noreply.github.com>
2024-08-20 21:27:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = flask.Flask(__name__)
 def index():
     return f'!909890 Hello World! {datetime.now()} version 21.03.2024'
 
+@app.route('/a1')
+def a1():
+    return f'a1 route now {datetime.now()} version 20.08.2024'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
