b70ff1499cb71e73c627bbe6abe823da11cce07c
Serhii Sheiko <ssic7i@gmail.com>
2025-02-20 21:27:23 +0200

    1

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Hello World! {datetime.now()} version 17.02.2025'
+    return f'Hello World! {datetime.now()} version 17.02.2025 v2  фівфі '
 
 @app.route('/a1')
 def a1():
