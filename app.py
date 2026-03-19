3d8812fbaa9526b1f44f85766789269c581898d6
Serhii Sheiko <128324762+serhii-sheiko@users.noreply.github.com>
2026-03-19 20:45:46 +0200

    Update greeting message in index route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Hello World! {datetime.now()} version 17.02.2025 v2  fffff '
+    return f'Hello World! {datetime.now()} version 17.02.2025 kkkkkkkkkkk'
 
 @app.route('/a1')
 def a1():
